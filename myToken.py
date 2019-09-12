40d357d146a750a1e393570e6b3d78df6ec7d2f2
Jon wick <43663480+bboychris168@users.noreply.github.com>
2019-09-12 13:16:24 +1000

    Create myToken.py

---
+++ myToken.py
@@ -0,0 +1,10 @@
+token = ''
+# PLEASE USE INTEGERS NOT STRINGS. Grab the server ID and channel IDs for the channels you wish to use. 
+guildID = []
+team1ChannelId = []
+team2ChannelId = []
+setupChannelId = []
+csRoleID = []
+datHostPass = " "
+datHostUser = ""
+serverId = ""
